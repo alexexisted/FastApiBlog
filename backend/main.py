ef075d37d26c53f0e16473d98d3c47405f79b3e6
alexexisted <a.assjones@list.ru>
2024-01-09 20:39:19 +0300

    added psql db, made docker-compose file

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from backend.core.config import settings
+from backend.db.base_class import Base
+from backend.db.session import engine
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
+
+
+@app.get("/")
+def home():
+    return {"msg":"Hello FastAPI"}
+
+
