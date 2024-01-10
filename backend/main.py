d4247cdfad4d0b77f3d73152c2b8cb02268eec99
alexexisted <a.assjones@list.ru>
2024-01-10 20:30:35 +0300

    added route to endpoint to create a user

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,21 @@ from backend.core.config import settings
 from backend.db.base import Base
 from backend.db.session import engine
 
+from backend.apis.base import api_router
+
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
+def include_router(app):
+    app.include_router(api_router)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 
