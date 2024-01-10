a368f9178915c10694eea1fd7369f86d671e3786
alexexisted <a.assjones@list.ru>
2024-01-11 00:42:08 +0300

    made a function to create a blog, using CreateBlog schema from schemas/blog.py. Commited and refreshed db

---
+++ backend/db/repository/blog.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from backend.schemas.blog import CreateBlog
+from backend.db.models.blog import Blog
+
+
+def create_new_blog(blog: CreateBlog, db: Session, author_id: int = 1):
+    blog = Blog(**blog.model_dump(), author_id=author_id)
+    db.add(blog)
+    db.commit()
+    db.refresh(blog)
+    return blog
