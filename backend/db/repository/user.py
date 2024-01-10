6200b2be1b5f30bbe68a910b7a1647c40bc40996
alexexisted <a.assjones@list.ru>
2024-01-10 20:32:25 +0300

    added new function which create new user, using UserCreate schema and pydantic validation

---
+++ backend/db/repository/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+from backend.schemas.user import UserCreate
+from backend.db.models.user import User
+from backend.core.hashing import Hasher
+
+
+def create_new_user(user: UserCreate, db: Session):
+    user = User(
+        email = user.email,
+        password = Hasher.get_password_hash(user.password),
+        is_active = True,
+        is_superuser = False
+        )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
