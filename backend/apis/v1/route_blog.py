1090153000c3bce76480d9fe93875563e33c4a12
alexexisted <a.assjones@list.ru>
2024-01-12 22:40:13 +0300

    added three routers to: 1. get some blog by ID. Used response model ShowBlog from schemas/blog.py 2. get the list of blogs from db. 3. put route to update a blog All functions in routes were written in repository/blog.py for simplified code support

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from backend.db.session import get_db
-from backend.schemas.blog import ShowBlog, CreateBlog
-from backend.db.repository.blog import create_new_blog
+from backend.schemas.blog import ShowBlog, CreateBlog, UpdateBlog
+from backend.db.repository.blog import create_new_blog, retreive_blog, list_blogs, update_blog
+
+from typing import List
 
 router = APIRouter()
 
@@ -12,3 +14,28 @@ router = APIRouter()
 async def create_blog(blog: CreateBlog, db: Session = Depends(get_db)):
     blog = create_new_blog(blog=blog, db=db, author_id=1)
     return blog
+
+
+@router.get("/blog/{id}", response_model=ShowBlog)
+async def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = retreive_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(detail=f"Blog with id {id} does not exist")
+    return blog
+
+
+@router.get("/blogs", response_model=List[ShowBlog])
+async def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = list_blogs(db=db)
+    return blogs
+
+
+@router.put("/blog/{id}", response_model=ShowBlog)
+def update_a_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db)):
+    blog = update_blog(id=id, blog=blog, db=db, author_id=1)
+    if not blog:
+        raise HTTPException(detail=f"Blog with this id: {id} does not exist")
+    return blog
+
+
+
