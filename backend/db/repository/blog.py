2c4e27f4f672a329bfabf63dd69c695d181c7389
alexexisted <a.assjones@list.ru>
2024-01-12 22:30:30 +0300

    added funcs to get some blog by ID, to get list of blogs and to update blog.

--- backend/db/repository/blog.py
+++ backend/db/repository/blog.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from backend.schemas.blog import CreateBlog
+from backend.schemas.blog import CreateBlog, UpdateBlog
 from backend.db.models.blog import Blog
 
 
@@ -9,3 +9,24 @@ def create_new_blog(blog: CreateBlog, db: Session, author_id: int = 1):
     db.commit()
     db.refresh(blog)
     return blog
+
+
+def retreive_blog(id: int, db: Session):
+    blog = db.query(Blog).filter(Blog.id == id).first()
+    return blog
+
+
+def list_blogs(db: Session):
+    blogs = db.query(Blog).filter(Blog.is_active == True).all()
+    return blogs
+
+
+def update_blog(id: int, blog: UpdateBlog, author_id: int, db: Session):
+    blog_in_db = db.query(Blog).filter(Blog.id == id).first()
+    if not blog_in_db:
+        return
+    blog_in_db.title = blog.title
+    blog_in_db.content = blog.content
+    db.add(blog_in_db)
+    db.commit()
+    return blog_in_db
