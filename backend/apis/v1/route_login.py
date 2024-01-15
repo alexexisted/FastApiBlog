822283c25cc7296649f98de9e8ece2585096cd04
alexexisted <a.assjones@list.ru>
2024-01-15 17:35:07 +0300

    added authenticate endpoint and function to verify the password.

---
+++ backend/apis/v1/route_login.py
@@ -0,0 +1,34 @@
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends, APIRouter, status, HTTPException
+from sqlalchemy.orm import Session
+from backend.db.session import get_db
+from backend.core.hashing import Hasher
+from backend.db.repository.login import get_user
+from backend.core.security import create_access_token
+from backend.schemas.token import Token
+
+router = APIRouter()
+
+
+def authenticate_user(email: str, password: str, db: Session):
+    user = get_user(email=email, db=db)
+    print(user)
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.password):
+        return False
+    return user
+
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token = create_access_token(
+        data={"sub": user.email}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
