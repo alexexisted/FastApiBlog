a4ff1fc08a2c93499cfd31a6995ba59a58ccd4cc
alexexisted <a.assjones@list.ru>
2024-01-18 22:23:25 +0300

    added route to sign up using retrieve_blog function and used html file as a response

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from typing import Optional
 from sqlalchemy.orm import Session
 from backend.db.session import get_db
-from backend.db.repository.blog import list_blogs
+from backend.db.repository.blog import list_blogs, retrieve_blog
 
 
 templates = Jinja2Templates(directory="backend/templates")
@@ -10,6 +11,16 @@ router = APIRouter()
 
 
 @router.get("/")
-def home(request: Request, db: Session = Depends(get_db)):
+def home(request: Request, alert: Optional[str] = None, db: Session = Depends(get_db)):
     blogs = list_blogs(db=db)
-    return templates.TemplateResponse("blog/home.html", {"request": request, "blogs": blogs})
+    return templates.TemplateResponse("blog/home.html", {"request": request, "blogs": blogs, "alert": alert})
+
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    return templates.TemplateResponse(
+        "blog/detail.html", {"request": request, "blog": blog}
+    )
+
+
