8f0b0b0e595d33c3049d367877cd12508e4fc96c
alexexisted <a.assjones@list.ru>
2024-01-18 22:24:23 +0300

    added route to sign up using retrieve_blog function and used html file as a response

---
+++ backend/apps/v1/route_login.py
@@ -0,0 +1,37 @@
+import json
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi import responses, status, Form
+from sqlalchemy.orm import Session
+from pydantic.error_wrappers import ValidationError
+
+from backend.db.session import get_db
+from backend.schemas.user import UserCreate
+from backend.db.repository.user import create_new_user
+
+templates = Jinja2Templates(directory="backend/templates")
+router = APIRouter()
+
+@router.get("/register")
+def register(request: Request):
+    return templates.TemplateResponse(
+        "auth/register.html", {"request": request}
+    )
+
+
+@router.post("/register")
+def register(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    errors = []
+    try:
+        user = UserCreate(email=email, password=password)
+        create_new_user(user=user, db=db)
+        return responses.RedirectResponse("/?alert=Successfully%20Registered", status_code=status.HTTP_302_FOUND)
+    except ValidationError as e:
+        errors_list = json.loads(e.json())
+        for item in errors_list:
+            errors.append(item.get("loc")[0] + ':' + item.get("msg"))
+    return templates.TemplateResponse(
+        "auth/register.html", {"request": request, "errors": errors}
+    )
+
+
