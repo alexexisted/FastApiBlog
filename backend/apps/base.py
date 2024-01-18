b16324e162cd09b5fb8221c988fa36fba0074021
alexexisted <a.assjones@list.ru>
2024-01-18 22:18:35 +0300

    added new router of login_route

--- backend/apps/base.py
+++ backend/apps/base.py
@@ -1,8 +1,12 @@
-from backend.apps.v1 import route_blog
+from backend.apps.v1 import route_blog, route_login
 from fastapi import APIRouter
 
 app_router = APIRouter()
 
 app_router.include_router(
     route_blog.router, prefix="", tags=["blogs"], include_in_schema=False
+)
+
+app_router.include_router(
+    route_login.router, prefix="", tags=["login"], include_in_schema=False
 )
