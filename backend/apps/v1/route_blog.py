af1d9d6616c161a6477afdd6c7b704e0ffd07eb6
alexexisted <a.assjones@list.ru>
2024-01-20 19:23:59 +0300

    added routes to create a blog and delete it if the user is an owner

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -5,6 +5,13 @@ from sqlalchemy.orm import Session
 from backend.db.session import get_db
 from backend.db.repository.blog import list_blogs, retrieve_blog
 
+from fastapi import Form, responses, status
+from fastapi.security.utils import get_authorization_scheme_param
+from backend.schemas.blog import CreateBlog
+from backend.db.repository.blog import create_new_blog
+from backend.apis.v1.route_login import get_current_user
+from backend.db.repository.blog import delete_blog
+
 
 templates = Jinja2Templates(directory="backend/templates")
 router = APIRouter()
@@ -24,6 +31,52 @@ def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
     )
 
 
+@router.get("/app/create-new-blog")
+def create_blog(request: Request):
+    return templates.TemplateResponse(
+        "blog/create_blog.html", {"request": request}
+    )
 
 
+@router.post("app/create-new-blog")
+def create_blog(
+        request: Request,
+        title: str = Form(...),
+        content: str = Form(...),
+        db: Session = Depends(get_db)):
+    token = request.cookies.get("access_token")
+    _, token = get_authorization_scheme_param(token)
+    try:
+        author = get_current_user(token=token, db=db)
+        blog = CreateBlog(title=title, content = content)
+        blog = create_new_blog(blog=blog, db=db, author_id=author.id)
+        return responses.RedirectResponse(
+            "/?alert=Blog Submitted for Review", status_code=status.HTTP_302_FOUND
+        )
+    except Exception as e:
+        errors = ["Please log in to create a blog"]
+        print("exeption raised:", e)
+        return templates.TemplateResponse(
+            "blog/create_blog.html", {"request": request, "errors": errors, "title": title, "content": content}
+        )
 
+
+@router.get("/delete/{id}")
+def delete_a_blog(request: Request,
+                  id: int,
+                  db: Session = Depends(get_db)):
+    token = request.cookies.get("access_token")
+    _, token = get_authorization_scheme_param(token)
+    try:
+        author = get_current_user(token=token, db=db)
+        msg = delete_blog(id=id, db=db, author_id=author.id)
+        alert = msg.get("error") or msg.get("msg")
+        return responses.RedirectResponse(
+            f"/?alert={alert}", status_code=status.HTTP_302_FOUND
+        )
+    except Exception as e:
+        print(f"Exception raised while deleting {e}")
+        blog = retrieve_blog(id=id, db=db)
+        return templates.TemplateResponse(
+            "blog/detail.html", {"request": request, "alert": "Please Login Again", "blog": blog}
+        )
